1871549cb9aa7df08ad3fc2c1f2be0466bcc365e
alchemistmatt <matthew.monroe@pnnl.gov>
2016-02-03 14:35:02 -0800

    Include the line number for every function in the stack trace Add function GetExceptionStackTraceData to clsStackTraceFormatter

--- clsStackTraceFormatter.vb
+++ clsStackTraceFormatter.vb
@@ -5,10 +5,13 @@ Imports System.Collections.Generic
 Imports System.Text
 Imports System.Text.RegularExpressions
 
+''' <summary>
+''' This class produces an easier-to read stack trace for an exception
+''' See the descriptions for functions GetExceptionStackTrace and 
+''' GetExceptionStackTraceMultiLine for example text
+''' </summary>
+''' <remarks>To see accurate source code line numbers you may need to compile with option "Enable Optimizations" disabled</remarks>
 Public Class clsStackTraceFormatter
-
-    ' Note: to see accurate line numbers in the stack trace, 
-    '       be sure to compile with option "Enable Optimizations" disabled
 
     Public Const STACK_TRACE_TITLE As String = "Stack trace: "
     Public Const STACK_CHAIN_SEPARATOR As String = "-:-"
@@ -72,19 +75,57 @@ Public Class clsStackTraceFormatter
 
     End Function
 
-    Private Shared Function GetExceptionStackTraceData(objException As Exception) As IEnumerable(Of String)
+    ''' <summary>
+    ''' Parses the StackTrace text of the given exception to return a cleaned up description of the current stack
+    ''' </summary>
+    ''' <param name="ex">Exception</param>
+    ''' <returns>
+    ''' List of function names; for example:
+    '''   clsCodeTest.Test
+    '''   clsCodeTest.TestException
+    '''   clsCodeTest.InnerTestException 
+    '''    in clsCodeTest.vb:line 86
+    ''' </returns>
+    ''' <remarks></remarks>
+    Public Shared Function GetExceptionStackTraceData(ex As Exception) As IEnumerable(Of String)
+        Return GetExceptionStackTraceData(ex.StackTrace)
+    End Function
+
+    ''' <summary>
+    ''' Parses the given StackTrace text to return a cleaned up description of the current stack
+    ''' </summary>
+    ''' <param name="stackTraceText">Exception.StackTrace data</param>
+    ''' <returns>
+    ''' List of function names; for example:
+    '''   clsCodeTest.Test
+    '''   clsCodeTest.TestException
+    '''   clsCodeTest.InnerTestException 
+    '''    in clsCodeTest.vb:line 86
+    ''' </returns>
+    ''' <remarks></remarks>
+    Public Shared Function GetExceptionStackTraceData(stackTraceText As String) As IEnumerable(Of String)
         Const REGEX_FUNCTION_NAME = "at ([^(]+)\("
         Const REGEX_FILE_NAME = "in .+\\(.+)"
+
+        Const CODE_LINE_PREFIX = ":line "
+        Const REGEX_LINE_IN_CODE = CODE_LINE_PREFIX & "\d+"
 
         Dim lstFunctions = New List(Of String)
         Dim strFinalFile As String = String.Empty
 
         Dim reFunctionName As New Regex(REGEX_FUNCTION_NAME, RegexOptions.Compiled Or RegexOptions.IgnoreCase)
         Dim reFileName As New Regex(REGEX_FILE_NAME, RegexOptions.Compiled Or RegexOptions.IgnoreCase)
+        Dim reLineInCode As New Regex(REGEX_LINE_IN_CODE, RegexOptions.Compiled Or RegexOptions.IgnoreCase)
+
+        If String.IsNullOrWhiteSpace(stackTraceText) Then
+            Dim emptyStackTrace = New List(Of String)
+            emptyStackTrace.Add("Empty stack trace")
+            Return emptyStackTrace
+        End If
 
         ' Process each line in objException.StackTrace
         ' Populate strFunctions() with the function name of each line
-        Using trTextReader = New StringReader(objException.StackTrace)
+        Using trTextReader = New StringReader(stackTraceText)
 
             Do While trTextReader.Peek > -1
                 Dim strLine = trTextReader.ReadLine()
@@ -93,6 +134,21 @@ Public Class clsStackTraceFormatter
                     Dim strCurrentFunction = String.Empty
 
                     Dim functionMatch = reFunctionName.Match(strLine)
+                    Dim lineMatch = reLineInCode.Match(strLine)
+
+                    ' Also extract the file name where the Exception occurred
+                    Dim fileMatch = reFileName.Match(strLine)
+                    Dim currentFunctionFile As String
+
+                    If fileMatch.Success Then
+                        currentFunctionFile = fileMatch.Groups(1).Value
+                        If strFinalFile.Length = 0 Then
+                            strFinalFile = String.Copy(currentFunctionFile)
+                        End If
+                    Else
+                        currentFunctionFile = String.Empty
+                    End If
+
                     If functionMatch.Success Then
                         strCurrentFunction = functionMatch.Groups(1).Value
                     Else
@@ -102,6 +158,7 @@ Public Class clsStackTraceFormatter
                             ' " in" not found; look for the first space after startIndex 4
                             intIndex = strLine.IndexOf(" ", 4, StringComparison.Ordinal)
                         End If
+
                         If intIndex = 0 Then
                             ' Space not found; use the entire string
                             intIndex = strLine.Length - 1
@@ -113,17 +170,22 @@ Public Class clsStackTraceFormatter
 
                     End If
 
-                    If Not String.IsNullOrEmpty(strCurrentFunction) Then
-                        lstFunctions.Add(strCurrentFunction)
-                    End If
+                    Dim functionDescription As String = String.Copy(strCurrentFunction)
 
-                    If strFinalFile.Length = 0 Then
-                        ' Also extract the file name where the Exception occurred
-                        Dim fileMatch = reFileName.Match(strLine)
-                        If fileMatch.Success Then
-                            strFinalFile = fileMatch.Groups(1).Value
+                    If Not String.IsNullOrEmpty(currentFunctionFile) Then
+                        If String.IsNullOrEmpty(strFinalFile) OrElse
+                           Not TrimLinePrefix(strFinalFile, CODE_LINE_PREFIX).Equals(
+                               TrimLinePrefix(currentFunctionFile, CODE_LINE_PREFIX),
+                               StringComparison.InvariantCulture) Then
+                            functionDescription &= FINAL_FILE_PREFIX & currentFunctionFile
                         End If
                     End If
+
+                    If lineMatch.Success AndAlso Not functionDescription.Contains(CODE_LINE_PREFIX) Then
+                        functionDescription &= lineMatch.Value
+                    End If
+
+                    lstFunctions.Add(functionDescription)
 
                 End If
             Loop
@@ -140,6 +202,16 @@ Public Class clsStackTraceFormatter
 
         Return stackTraceData
 
+    End Function
+
+    Private Shared Function TrimLinePrefix(strFileDescription As String, codeLinePrefix As String) As String
+
+        Dim matchIndex = strFileDescription.IndexOf(codeLinePrefix, StringComparison.Ordinal)
+        If matchIndex > 0 Then
+            Return strFileDescription.Substring(0, matchIndex)
+        End If
+
+        Return strFileDescription
     End Function
 
 End Class
