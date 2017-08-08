74aca61d0b4362f7d9774e8b96c24cdd9bb10985
alchemistmatt <matthew.monroe@pnnl.gov>
2017-08-07 18:22:20 -0700

    Add expected values to the CompactPath unit test

--- UnitTests/PathTests.cs
+++ UnitTests/PathTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using NUnit.Framework;
 using PRISM;
 
@@ -8,29 +9,31 @@ namespace PRISMTest
     public class PathTests
     {
         [Test]
-        [TestCase(@"C:\temp", 10)]
-        [TestCase(@"C:\temp", 20)]
-        [TestCase(@"C:\temp\DataFile.txt", 10)]
-        [TestCase(@"C:\temp\DataFile.txt", 20)]
-        [TestCase(@"C:\temp\DataFile.txt", 30)]
-        [TestCase(@"C:\Program Files\Beyond Compare 4\BCompare.exe", 40)]
-        [TestCase(@"C:\Program Files\Beyond Compare 4\BCompare.exe", 50)]
-        [TestCase(@"C:\Program Files\Beyond Compare 4\BCompare.exe", 60)]
-        [TestCase(@"\\proto-6\dms_programs\AnalysisToolManager1\Logs", 40)]
-        [TestCase(@"\\proto-6\dms_programs\AnalysisToolManager1\Logs\", 40)]
-        [TestCase(@"\\proto-6\dms_programs\AnalysisToolManager1\Logs", 50)]
-        [TestCase(@"\\proto-6\dms_programs\AnalysisToolManager1\Logs", 60)]
-        [TestCase(@"\\proto-6\dms_programs\AnalysisToolManager1\Logs\", 60)]
-        [TestCase(@"\\proto-6\dms_programs\AnalysisToolManager1\Logs\AnalysisMgr_01-15-2017.txt", 40)]
-        [TestCase(@"\\proto-6\dms_programs\AnalysisToolManager1\Logs\AnalysisMgr_01-15-2017.txt", 60)]
-        [TestCase(@"\\proto-6\dms_programs\AnalysisToolManager1\Logs\AnalysisMgr_01-15-2017.txt", 80)]
-        [TestCase(@"\\proto-6\dms_programs\AnalysisToolManager1\Logs\AnalysisMgr_01-15-2017.txt", 100)]
-        [TestCase(@"\\proto-6\dms_programs\AnalysisToolManager1\Logs\AnalysisMgr_01-15-2017.txt", 120)]
-        public void CompactPath(string pathToCompact, int maxLength)
+        [TestCase(@"C:\temp", 10, @"C:\temp")]
+        [TestCase(@"C:\temp", 20, @"C:\temp")]
+        [TestCase(@"C:\temp\DataFile.txt", 10, @"C:\t..\Dat..")]
+        [TestCase(@"C:\temp\DataFile.txt", 20, @"C:\temp\DataFile.txt")]
+        [TestCase(@"C:\temp\DataFile.txt", 30, @"C:\temp\DataFile.txt")]
+        [TestCase(@"C:\Program Files\Beyond Compare 4\BCompare.exe", 40, @"C:\Progr..\Beyond Compare 4\BCompare.exe")]
+        [TestCase(@"C:\Program Files\Beyond Compare 4\BCompare.exe", 50, @"C:\Program Files\Beyond Compare 4\BCompare.exe")]
+        [TestCase(@"C:\Program Files\Beyond Compare 4\BCompare.exe", 60, @"C:\Program Files\Beyond Compare 4\BCompare.exe")]
+        [TestCase(@"\\proto-6\dms_programs\AnalysisToolManager1\Logs", 40, @"\\proto-6\dm..\AnalysisToolManager1\Logs")]
+        [TestCase(@"\\proto-6\dms_programs\AnalysisToolManager1\Logs\", 40, @"\\proto-6\d..\AnalysisToolManager1\Logs\")]
+        [TestCase(@"\\proto-6\dms_programs\AnalysisToolManager1\Logs", 50, @"\\proto-6\dms_programs\AnalysisToolManager1\Logs")]
+        [TestCase(@"\\proto-6\dms_programs\AnalysisToolManager1\Logs", 60, @"\\proto-6\dms_programs\AnalysisToolManager1\Logs")]
+        [TestCase(@"\\proto-6\dms_programs\AnalysisToolManager1\Logs\", 60, @"\\proto-6\dms_programs\AnalysisToolManager1\Logs\")]
+        [TestCase(@"\\proto-6\dms_programs\AnalysisToolManager1\Logs\AnalysisMgr_01-15-2017.txt", 40, @"\\p..\...\L..\AnalysisMgr_01-15-2017.txt")]
+        [TestCase(@"\\proto-6\dms_programs\AnalysisToolManager1\Logs\AnalysisMgr_01-15-2017.txt", 60, @"\\proto-6\...\AnalysisTool..\Logs\AnalysisMgr_01-15-2017.txt")]
+        [TestCase(@"\\proto-6\dms_programs\AnalysisToolManager1\Logs\AnalysisMgr_01-15-2017.txt", 80, @"\\proto-6\dms_programs\AnalysisToolManager1\Logs\AnalysisMgr_01-15-2017.txt")]
+        [TestCase(@"\\proto-6\dms_programs\AnalysisToolManager1\Logs\AnalysisMgr_01-15-2017.txt", 100, @"\\proto-6\dms_programs\AnalysisToolManager1\Logs\AnalysisMgr_01-15-2017.txt")]
+        [TestCase(@"\\proto-6\dms_programs\AnalysisToolManager1\Logs\AnalysisMgr_01-15-2017.txt", 120, @"\\proto-6\dms_programs\AnalysisToolManager1\Logs\AnalysisMgr_01-15-2017.txt")]
+        public void CompactPath(string pathToCompact, int maxLength, string expectedResult)
         {
             var shortPath = clsFileTools.CompactPathString(pathToCompact, maxLength);
 
             Console.WriteLine(shortPath);
+
+            Assert.AreEqual(expectedResult, shortPath, "Unexpected short path for {0}: {1}", pathToCompact, shortPath);
         }
     }
 }
