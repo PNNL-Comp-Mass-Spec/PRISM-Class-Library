c460e8b8c062e9020477a03a5a2ab27d4c694f29
Matthew Monroe <matt@alchemistmatt.com>
2017-08-13 17:30:02 -0700

    Change server update logic back to using the second Tuesday of the month

--- clsWindowsUpdateStatus.cs
+++ clsWindowsUpdateStatus.cs
@@ -5,7 +5,7 @@ namespace PRISM
     /// <summary>
     /// Utility functions for checking whether Windows updates are likely to be applied close to the current time
     /// Windows desktop computers have Windows updates applied around 3 am on the first Thursday after the third Tuesday of the month
-    /// Windows servers have Windows updates applied around 3 am or 10 am on the first Sunday after the third Tuesday of the month
+    /// Windows servers have Windows updates applied around 3 am or 10 am on the first Sunday after the second Tuesday of the month
     /// </summary>
     /// <remarks></remarks>
     public class clsWindowsUpdateStatus
@@ -44,7 +44,7 @@ namespace PRISM
         {
 
             // Determine the third Tuesday in the current month
-            var thirdTuesdayInMonth = GetThirdTuesdayInMonth(currentTime);
+            var thirdTuesdayInMonth = GetNthTuesdayInMonth(currentTime, 3);
 
             // Windows 7 / Windows 8 Pubs install updates around 3 am on the Thursday after the third Tuesday of the month
             // Return true between 12 am and 6:30 am on Thursday in the week with the third Tuesday of the month
@@ -67,6 +67,8 @@ namespace PRISM
                 return true;
             }
 
+            // No processing box updates are scheduled
+            // Check for server updates
             var pendingUpdates = ServerUpdatesArePending(currentTime, out pendingWindowsUpdateMessage);
 
             return pendingUpdates;
@@ -96,23 +98,22 @@ namespace PRISM
 
             pendingWindowsUpdateMessage = "No pending update";
 
-            // Determine the third Tuesday in the current month
-            var thirdTuesdayInMonth = GetThirdTuesdayInMonth(currentTime);
+            // Determine the second Tuesday in the current month
+            var secondTuesdayInMonth = GetNthTuesdayInMonth(currentTime, 2);
 
-            // Windows servers install updates around either 3 am or 10 am on the first Sunday after the third Tuesday of the month
-            // Return true between 2 am and 6:30 am or between 9:30 am and 11 am on the first Sunday after the third Tuesday of the month
-            var dtExclusionStart = thirdTuesdayInMonth.AddDays(5).AddHours(2);
-            var dtExclusionEnd = thirdTuesdayInMonth.AddDays(5).AddHours(6).AddMinutes(30);
+            // Windows servers install updates around either 3 am or 10 am on the first Sunday after the second Tuesday of the month
+            // Return true between 2 am and 6:30 am or between 9:30 am and 11 am on the first Sunday after the second Tuesday of the month
+            var dtExclusionStart = secondTuesdayInMonth.AddDays(5).AddHours(2);
+            var dtExclusionEnd = secondTuesdayInMonth.AddDays(5).AddHours(6).AddMinutes(30);
 
-            var dtExclusionStart2 = thirdTuesdayInMonth.AddDays(5).AddHours(9).AddMinutes(30);
-            var dtExclusionEnd2 = thirdTuesdayInMonth.AddDays(5).AddHours(11);
-
+            var dtExclusionStart2 = secondTuesdayInMonth.AddDays(5).AddHours(9).AddMinutes(30);
+            var dtExclusionEnd2 = secondTuesdayInMonth.AddDays(5).AddHours(11);
 
             if (currentTime >= dtExclusionStart && currentTime < dtExclusionEnd ||
                 currentTime >= dtExclusionStart2 && currentTime < dtExclusionEnd2)
             {
-                var dtPendingUpdateTime1 = thirdTuesdayInMonth.AddDays(5).AddHours(3);
-                var dtPendingUpdateTime2 = thirdTuesdayInMonth.AddDays(5).AddHours(10);
+                var dtPendingUpdateTime1 = secondTuesdayInMonth.AddDays(5).AddHours(3);
+                var dtPendingUpdateTime2 = secondTuesdayInMonth.AddDays(5).AddHours(10);
 
                 var pendingUpdateTimeText = dtPendingUpdateTime1.ToString("hh:mm:ss tt") + " or " + dtPendingUpdateTime2.ToString("hh:mm:ss tt");
 
@@ -132,7 +133,13 @@ namespace PRISM
 
         }
 
-        private static DateTime GetThirdTuesdayInMonth(DateTime currentTime)
+        /// <summary>
+        /// Return the first, second, third, fourth, or fifth Tuesday in the month
+        /// </summary>
+        /// <param name="currentTime"></param>
+        /// <param name="occurrence">1 for the first Tuesday, 2 for the second, etc.</param>
+        /// <returns></returns>
+        private static DateTime GetNthTuesdayInMonth(DateTime currentTime, int occurrence)
         {
             var candidateDate = new DateTime(currentTime.Year, currentTime.Month, 1);
             while (candidateDate.DayOfWeek != DayOfWeek.Tuesday)
@@ -140,9 +147,14 @@ namespace PRISM
                 candidateDate = candidateDate.AddDays(1);
             }
 
-            var thirdTuesdayInMonth = candidateDate.AddDays(14);
+            var addon = occurrence * 7 - 7;
+            if (addon == 0)
+            {
+                return candidateDate;
+            }
 
-            return thirdTuesdayInMonth;
+            var targetTuesday = candidateDate.AddDays(addon);
+            return targetTuesday;
         }
 
     }
