9fd49f2c26c6615d4d2bea5793655bbca2bf1ebd
alchemistmatt <matthew.monroe@pnnl.gov>
2017-10-18 18:44:32 -0700

    clsEventNotifier events now display messages at the console if there are no listeners
    Disable with WriteToConsoleIfNoListener or the SkipConsoleWrite properties

--- clsEventNotifier.cs
+++ clsEventNotifier.cs
@@ -9,7 +9,7 @@ namespace PRISM
     public abstract class clsEventNotifier
     {
 
-        #region "Events and Event Handlers"
+        #region "Events and Delegates"
 
         /// <summary>
         /// Debug event
@@ -68,12 +68,80 @@ namespace PRISM
         /// <param name="message"></param>
         public delegate void WarningEventEventHandler(string message);
 
+        #endregion
+
+        #region "Properties"
+
+        /// <summary>
+        /// If WriteToConsoleIfNoListener is true, optionally set this to true to not write debug messages to the console if no listener
+        /// </summary>
+        public bool SkipConsoleWriteIfNoDebugListener { get; set; }
+
+        /// <summary>
+        /// If WriteToConsoleIfNoListener is true, optionally set this to true to not write errors to the console if no listener
+        /// </summary>
+        public bool SkipConsoleWriteIfNoErrorListener { get; set; }
+
+        /// <summary>
+        /// If WriteToConsoleIfNoListener is true, optionally set this to true to not write progress updatess to the console if no listener
+        /// </summary>
+        public bool SkipConsoleWriteIfNoProgressListener { get; set; }
+
+        /// <summary>
+        /// If WriteToConsoleIfNoListener is true, optionally set this to true to not write status messages to the console if no listener
+        /// </summary>
+        public bool SkipConsoleWriteIfNoStatusListener { get; set; }
+
+        /// <summary>
+        /// If WriteToConsoleIfNoListener is true, optionally set this to true to not write warnings to the console if no listener
+        /// </summary>
+        public bool SkipConsoleWriteIfNoWarningListener { get; set; }
+
+        /// <summary>
+        /// If true, and if an event does not have a listener, display the message at the console
+        /// </summary>
+        /// <remarks>Defaults to true. Silence individual event types using the SkipConsoleWrite properties</remarks>
+        public bool WriteToConsoleIfNoListener { get; set; } = true;
+
+        /// <summary>
+        /// True if the Debug event has any listeners
+        /// </summary>
+        protected bool HasEventListenerDebug => DebugEvent != null;
+
+        /// <summary>
+        /// True if the Error event has any listeners
+        /// </summary>
+        protected bool HasEventListenerError => ErrorEvent != null;
+
+        /// <summary>
+        /// True if the ProgressUpdate event has any listeners
+        /// </summary>
+        protected bool HasEventListenerProgressUpdate => ProgressUpdate != null;
+
+        /// <summary>
+        /// True if the StatusEvent event has any listeners
+        /// </summary>
+        protected bool HasEventListenerStatusEvent => StatusEvent != null;
+
+        /// <summary>
+        /// True if the WarningEvent event has any listeners
+        /// </summary>
+        protected bool HasEventListenerWarningEvent => WarningEvent != null;
+
+        #endregion
+
+        #region "Event Handlers"
+
         /// <summary>
         /// Report a debug message
         /// </summary>
         /// <param name="message"></param>
         protected void OnDebugEvent(string message)
         {
+            if (DebugEvent == null && WriteToConsoleIfNoListener && !SkipConsoleWriteIfNoDebugListener)
+            {
+                ConsoleMsgUtils.ShowDebug(message);
+            }
             DebugEvent?.Invoke(message);
         }
 
@@ -83,6 +151,10 @@ namespace PRISM
         /// <param name="message"></param>
         protected void OnErrorEvent(string message)
         {
+            if (ErrorEvent == null && WriteToConsoleIfNoListener && !SkipConsoleWriteIfNoErrorListener)
+            {
+                ConsoleMsgUtils.ShowError(message, false, false);
+            }
             ErrorEvent?.Invoke(message, null);
         }
 
@@ -93,6 +165,10 @@ namespace PRISM
         /// <param name="ex">Exception (allowed to be nothing)</param>
         protected void OnErrorEvent(string message, Exception ex)
         {
+            if (ErrorEvent == null && WriteToConsoleIfNoListener && !SkipConsoleWriteIfNoErrorListener)
+            {
+                ConsoleMsgUtils.ShowError(message, ex, false, false);
+            }
             ErrorEvent?.Invoke(message, ex);
         }
 
@@ -103,6 +179,10 @@ namespace PRISM
         /// <param name="percentComplete">Value between 0 and 100</param>
         protected void OnProgressUpdate(string progressMessage, float percentComplete)
         {
+            if (ProgressUpdate == null && WriteToConsoleIfNoListener && !SkipConsoleWriteIfNoProgressListener)
+            {
+                Console.WriteLine("{0:F2}%: {1}", percentComplete, progressMessage);
+            }
             ProgressUpdate?.Invoke(progressMessage, percentComplete);
         }
 
@@ -112,6 +192,10 @@ namespace PRISM
         /// <param name="message"></param>
         protected void OnStatusEvent(string message)
         {
+            if (StatusEvent == null && WriteToConsoleIfNoListener && !SkipConsoleWriteIfNoStatusListener)
+            {
+                Console.WriteLine(message);
+            }
             StatusEvent?.Invoke(message);
         }
 
@@ -121,22 +205,27 @@ namespace PRISM
         /// <param name="message"></param>
         protected void OnWarningEvent(string message)
         {
+            if (WarningEvent == null && WriteToConsoleIfNoListener && !SkipConsoleWriteIfNoWarningListener)
+            {
+                ConsoleMsgUtils.ShowWarning(message);
+            }
             WarningEvent?.Invoke(message);
         }
+
+        #endregion
 
         /// <summary>
         /// Use this method to chain events between classes
         /// </summary>
-        /// <param name="oProcessingClass"></param>
-        protected void RegisterEvents(clsEventNotifier oProcessingClass)
+        /// <param name="sourceClass"></param>
+        protected void RegisterEvents(clsEventNotifier sourceClass)
         {
-            oProcessingClass.DebugEvent += OnDebugEvent;
-            oProcessingClass.StatusEvent += OnStatusEvent;
-            oProcessingClass.ErrorEvent += OnErrorEvent;
-            oProcessingClass.WarningEvent += OnWarningEvent;
-            oProcessingClass.ProgressUpdate += OnProgressUpdate;
+            sourceClass.DebugEvent += OnDebugEvent;
+            sourceClass.StatusEvent += OnStatusEvent;
+            sourceClass.ErrorEvent += OnErrorEvent;
+            sourceClass.WarningEvent += OnWarningEvent;
+            sourceClass.ProgressUpdate += OnProgressUpdate;
         }
 
-        #endregion
     }
 }
