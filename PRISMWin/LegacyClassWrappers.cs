de32f00e5d0d3a1536f3ad2945229d11201fd41e
alchemistmatt <matthew.monroe@pnnl.gov>
2018-09-20 15:47:09 -0700

    Add legacy class wrappers

---
+++ PRISMWin/LegacyClassWrappers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PRISMWin
+{
+    [Obsolete("Use the DiskInfo class")]
+    public class clsDiskInfo : DiskInfo
+    {
+    }
+
+    [Obsolete("Use the DotNETVersionChecker class")]
+    public class clsDotNETVersionChecker : DotNETVersionChecker
+    {
+    }
+
+    [Obsolete("Use the ProcessStats class")]
+    public class clsProcessStats : ProcessStats
+    {
+        public clsProcessStats(bool limitLoggingByTimeOfDay = false) : base(limitLoggingByTimeOfDay)
+        {
+        }
+    }
+
+}
