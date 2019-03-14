828a30936a80aa9519a34695f3a0342b6a8cacb0
alchemistmatt <matthew.monroe@pnnl.gov>
2019-03-13 18:33:45 -0700

    Add class StringSimilarityTool and unit tests

---
+++ DataUtils/StringSimilarityTool.cs
@@ -0,0 +1,177 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PRISM.DataUtils
+{
+    // ReSharper disable once CommentTypo
+
+    /// <summary>
+    /// This class implements a string comparison algorithm based on character pair similarity
+    /// Original algorithm developed by Simon White in 1992: http://www.catalysoft.com/articles/StrikeAMatch.html
+    /// Converted to C# by Michael La Voie in 2009: https://stackoverflow.com/a/1663745/1179467
+    /// Expanded by Matthew Monroe in 2019 to support single letter words (or digits)
+    /// </summary>
+    public static class StringSimilarityTool
+    {
+        private static readonly Regex mAlphaNumericMatcher = new Regex("[a-z0-9]+", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        private static readonly Regex mLetterMatcher = new Regex("[a-z]+", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        private static readonly Regex mLetterWhitespaceMatcher = new Regex("[a-z \t\r\n]+", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        /// <summary>
+        /// Compares two strings based on letter pair matches
+        /// </summary>
+        /// <param name="text1"></param>
+        /// <param name="text2"></param>
+        /// <param name="removeNumbers">When true, remove digits from the text before comparing</param>
+        /// <param name="removeSymbolsAndWhitespace">When true, remove symbols (anything not a letter or number) and whitespace from the text before comparing</param>
+        /// <param name="caseSensitive">When true, require matching capitalization</param>
+        /// <returns>Similarity score, ranging from from 0.0 to 1.0 where 1.0 is a perfect match</returns>
+        public static double CompareStrings(
+            string text1,
+            string text2,
+            bool removeNumbers = false,
+            bool removeSymbolsAndWhitespace = true,
+            bool caseSensitive = false)
+        {
+            var pairs1 = WordLetterPairs(text1, removeNumbers, removeSymbolsAndWhitespace, caseSensitive);
+            var pairs2 = WordLetterPairs(text2, removeNumbers, removeSymbolsAndWhitespace, caseSensitive);
+
+            var intersection = 0;
+            var union = pairs1.Count + pairs2.Count;
+
+            if (union <= 0)
+                return 0;
+
+            foreach (var pair in pairs1)
+            {
+                for (var j = 0; j < pairs2.Count; j++)
+                {
+                    if (pair != pairs2[j])
+                        continue;
+
+                    intersection++;
+
+                    // ReSharper disable CommentTypo
+
+                    // Remove the match to prevent "GGGG" from appearing to match "GG" with 100% success
+                    pairs2.RemoveAt(j);
+
+                    // ReSharper restore CommentTypo
+
+                    break;
+                }
+            }
+
+            return 2.0 * intersection / union;
+        }
+
+        /// <summary>
+        /// Concatenate all RegEx matches in text
+        /// </summary>
+        /// <param name="matcher"></param>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        private static string CombineAllMatches(Regex matcher, string text)
+        {
+            var filteredText = new StringBuilder();
+
+            var match = matcher.Match(text);
+
+            while (match.Success)
+            {
+                filteredText.Append(match.Value);
+                match = match.NextMatch();
+            }
+
+            return filteredText.ToString();
+        }
+
+        /// <summary>
+        /// Generates an array containing every two consecutive letters in the input string
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns>List of pairs</returns>
+        /// <remarks>If the text is a single character, returns an array of length 1 with that single character</remarks>
+        private static IEnumerable<string> LetterPairs(string text)
+        {
+            if (text.Length < 1)
+                return new string[0];
+
+            if (text.Length == 1)
+            {
+                var singleChar = new string[1];
+                singleChar[0] = text;
+                return singleChar;
+            }
+
+            var numPairs = text.Length - 1;
+
+            var pairs = new string[numPairs];
+
+            for (var i = 0; i < numPairs; i++)
+            {
+                pairs[i] = text.Substring(i, 2);
+            }
+
+            return pairs;
+        }
+
+        /// <summary>
+        /// Gets all letter pairs for each individual word in the string
+        /// </summary>
+        /// <param name="textBob"></param>
+        /// <param name="removeNumbers">When true, remove digits from the text before comparing</param>
+        /// <param name="removeSymbolsAndWhitespace">When true, remove symbols (anything not a letter or number) and whitespace from the text before comparing</param>
+        /// <param name="caseSensitive">When true, require matching capitalization</param>
+        /// <returns>List of word letter pairs</returns>
+        private static List<string> WordLetterPairs(string textBob, bool removeNumbers = false, bool removeSymbolsAndWhitespace = true, bool caseSensitive = false)
+        {
+            var allPairs = new List<string>();
+
+            var textToCheck = caseSensitive ? textBob : textBob.ToUpper();
+            string filteredText;
+
+            if (removeSymbolsAndWhitespace)
+            {
+                var alphanumericText = CombineAllMatches(mAlphaNumericMatcher, textToCheck);
+
+                if (removeNumbers)
+                {
+                    filteredText = CombineAllMatches(mLetterMatcher, alphanumericText);
+                }
+                else
+                {
+                    filteredText = alphanumericText;
+                }
+            }
+            else if (removeNumbers)
+            {
+                filteredText = CombineAllMatches(mLetterWhitespaceMatcher, textToCheck);
+            }
+            else
+            {
+                filteredText = textToCheck;
+            }
+
+            // Tokenize the string and put the tokens/words into an array
+            var words = Regex.Split(filteredText ?? string.Empty, @"\s");
+
+            foreach (var word in words)
+            {
+                if (string.IsNullOrEmpty(word))
+                    continue;
+
+                // Find the pairs of characters
+                var pairsInWord = LetterPairs(word);
+
+                allPairs.AddRange(pairsInWord);
+            }
+
+            return allPairs;
+        }
+
+    }
+}
---
+++ UnitTests/StringSimilarityTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using NUnit.Framework;
+using PRISM.DataUtils;
+
+namespace PRISMTest
+{
+    [TestFixture]
+    class StringSimilarityTests
+    {
+
+        [Test]
+        [TestCase("Breadbox", "Redbox", 0.6666666, 0.6666666)]
+        [TestCase("CSharp", "Csharp", 1, 1)]
+        [TestCase("C# Code", "C# Code", 1, 1)]
+        [TestCase("C# 6 Code", "C# 7 Code", 0.8, 0.6)]
+        [TestCase("C# Code", "C#Code", 0.88888, 1)]
+        [TestCase("C# Code", "C#_Code", 0.8, 1)]
+        [TestCase(".NET 4.6.2", ".NET 4.6.1", 0.8571, 0.8)]
+        [TestCase("Abc", "XYZ", 0, 0)]
+        [TestCase("William", "George", 0, 0)]
+        [TestCase("William", "Bill", 0.4444, 0.4444)]
+        [TestCase("QC_Shew_18_02_Run-01_13Mar19_Valco_19-01-03", "QC_Shew_18_02_Run-02_13Mar19_Valco_19-01-03", 0.9524, 0.9375)]
+        [TestCase("QC_Shew_18_02_Run-01_13Mar19_Valco_19-01-03", "QC_Shew_18_02_Frac-08-of-11_12Mar19_Tiger_19-01-05", 0.6154, 0.5217)]
+        [TestCase("QC_Shew_16_01_Run-02_05Mar19_Valco_19-01-03", "QC_Shew_18_02-Run2_4Mar19_Arwen_18-11-02", 0.5185, 0.4516)]
+        [TestCase("QC_Shew_16_01_Run-02_05Mar19_Valco_19-01-03", "50	QC_Shew_18_02-Run2_4Mar19_Arwen_18-11-02", 0.5122, 0.4375)]
+        [TestCase("QC_Shew_16_01_Frac-10-of-11_26Feb19_Tiger_19-01-05", "QC_Shew_18_02-run2_01Mar19_Arwen_18-11-02", 0.3820, 0.3235)]
+        [TestCase("QC_Shew_18_02-run8_02Mar19_Arwen_18-11-02", "QC_Shew_18_02-run4_01Mar19_Arwen_18-11-02", 0.9, 0.8710)]
+        [TestCase("QC_Shew2_18-02_01Mar19_Merry_18-09-02", "QC_Shew1_18-02_01Mar19_Merry_18-09-02", 0.9444, 0.9286)]
+        public void CompareStringsWithNumbers(string text1, string text2, double expectedSimilarityScore, double expectedSimilarityScoreNoSymbolsOrWhitespace)
+        {
+            // Score with numbers, whitespace, and symbols
+            var similarityScore = StringSimilarityTool.CompareStrings(text1, text2, false, false);
+
+            // Score with numbers, but no whitespace or symbols
+            var similarityScoreNoSymbolsOrWhitespace = StringSimilarityTool.CompareStrings(text1, text2, false, true);
+
+            DisplayAndCompareScores(text1, text2,
+                                    similarityScore, similarityScoreNoSymbolsOrWhitespace,
+                                    expectedSimilarityScore, expectedSimilarityScoreNoSymbolsOrWhitespace);
+        }
+
+        [Test]
+        [TestCase("Breadbox", "Redbox", 0.6666666, 0.6666666)]
+        [TestCase("CSharp", "Csharp", 1, 1)]
+        [TestCase("C# Code", "C# Code", 1, 1)]
+        [TestCase("C# 6 Code", "C# 7 Code", 1, 1)]
+        [TestCase("C# Code", "C#Code", 0.75, 1)]
+        [TestCase("C# Code", "C#_Code", 0.75, 1)]
+        [TestCase(".NET 4.6.2", ".NET 4.6.1", 1, 1)]
+        [TestCase("Abc", "XYZ", 0, 0)]
+        [TestCase("William", "George", 0, 0)]
+        [TestCase("William", "Bill", 0.4444, 0.4444)]
+        [TestCase("QC_Shew_18_02_Run-01_13Mar19_Valco_19-01-03", "QC_Shew_18_02_Run-02_13Mar19_Valco_19-01-03", 1, 1)]
+        [TestCase("QC_Shew_18_02_Run-01_13Mar19_Valco_19-01-03", "QC_Shew_18_02_Frac-08-of-11_12Mar19_Tiger_19-01-05", 0.4571, 0.4571)]
+        [TestCase("QC_Shew_16_01_Run-02_05Mar19_Valco_19-01-03", "QC_Shew_18_02-Run2_4Mar19_Arwen_18-11-02", 0.6875, 0.6875)]
+        [TestCase("QC_Shew_16_01_Run-02_05Mar19_Valco_19-01-03", "50	QC_Shew_18_02-Run2_4Mar19_Arwen_18-11-02", 0.6875, 0.6875)]
+        [TestCase("QC_Shew_16_01_Frac-10-of-11_26Feb19_Tiger_19-01-05", "QC_Shew_18_02-run2_01Mar19_Arwen_18-11-02", 0.3235, -1)]
+        [TestCase("QC_Shew_18_02-run8_02Mar19_Arwen_18-11-02", "QC_Shew_18_02-run4_01Mar19_Arwen_18-11-02",1, 1)]
+        [TestCase("QC_Shew2_18-02_01Mar19_Merry_18-09-02", "QC_Shew1_18-02_01Mar19_Merry_18-09-02", 1, 1)]
+        public void CompareStringsNoNumbers(string text1, string text2, double expectedSimilarityScore, double expectedSimilarityScoreNoSymbolsOrWhitespace)
+        {
+            // Score without numbers but with whitespace, and symbols
+            var similarityScore = StringSimilarityTool.CompareStrings(text1, text2, true, false);
+
+            // Score without numbers, whitespace, or symbols
+            var similarityScoreNoSymbolsOrWhitespace = StringSimilarityTool.CompareStrings(text1, text2, true, true);
+
+            DisplayAndCompareScores(text1, text2,
+                                    similarityScore, similarityScoreNoSymbolsOrWhitespace,
+                                    expectedSimilarityScore, expectedSimilarityScoreNoSymbolsOrWhitespace);
+        }
+
+        private void DisplayAndCompareScores(string text1, string text2, double similarityScore, double similarityScoreNoSymbolsOrWhitespace, double expectedSimilarityScore, double expectedSimilarityScoreNoSymbolsOrWhitespace)
+        {
+
+            Console.WriteLine("With whitespace, similarity score is \n{0:F4} for \n{1} vs.\n{2}",
+                              similarityScore, text1, text2);
+            Console.WriteLine();
+
+            Console.WriteLine("Ignoring whitespace, similarity score is \n{0:F4} for \n{1} vs.\n{2}",
+                              similarityScoreNoSymbolsOrWhitespace, text1, text2);
+            Console.WriteLine();
+
+            if (expectedSimilarityScore < 0 || expectedSimilarityScoreNoSymbolsOrWhitespace < 0)
+            {
+                Console.WriteLine("Warning: Comparison skipped");
+                return;
+            }
+
+            Assert.AreEqual(expectedSimilarityScore, similarityScore, 0.0001,
+                            "Actual score of {0} does not match the expected score, {1}",
+                            similarityScore, expectedSimilarityScore);
+
+            Assert.AreEqual(expectedSimilarityScoreNoSymbolsOrWhitespace, similarityScoreNoSymbolsOrWhitespace, 0.0001,
+                            "Actual score of {0} does not match the expected score, {1} (ignore whitespace)",
+                            similarityScoreNoSymbolsOrWhitespace, expectedSimilarityScoreNoSymbolsOrWhitespace);
+
+        }
+    }
+
+}
