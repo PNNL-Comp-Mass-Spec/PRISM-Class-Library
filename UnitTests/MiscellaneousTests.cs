e701b45fefb2e906a71bc13db2246ca006235f40
alchemistmatt <matthew.monroe@pnnl.gov>
2019-09-19 10:46:21 -0700

    Fix unit tests

--- UnitTests/MiscellaneousTests.cs
+++ UnitTests/MiscellaneousTests.cs
@@ -10,19 +10,22 @@ namespace PRISMTest
         [TestCase("8/10/2017 2 am", false)]
         [TestCase("8/15/2017 2 am", false)]
         [TestCase("8/16/2017 11 pm", false)]
-        [TestCase("8/17/2017 12 am", true)]
-        [TestCase("8/17/2017 12:15 am", true)]
-        [TestCase("8/17/2017 1 am", true)]
-        [TestCase("8/17/2017 2 am", true)]
-        [TestCase("8/17/2017 6 am", true)]
+        [TestCase("8/17/2017 12 am", false)]        // Was true prior to September 2019
+        [TestCase("8/17/2017 12:15 am", false)]     // Was true prior to September 2019
+        [TestCase("8/17/2017 6 am", false)]         // Was true prior to September 2019
         [TestCase("8/17/2017 6:31 am", false)]
         [TestCase("8/17/2017 9 am", false)]
         [TestCase("8/18/2017 2 am", false)]
+        [TestCase("8/13/2017 2 am", true)]          // Server updates are pending
+        [TestCase("8/13/2017 3 am", true)]
+        [TestCase("8/13/2017 6:11 am", true)]
         public void TestUpdatesArePending(string dateToTest, bool expectedValue)
         {
-
             var currentDate = DateTime.Parse(dateToTest);
-            var pendingUpdates = WindowsUpdateStatus.UpdatesArePending(currentDate, out _);
+            var pendingUpdates = WindowsUpdateStatus.UpdatesArePending(currentDate, out var pendingWindowsUpdateMessage);
+
+            if (pendingUpdates)
+                Console.WriteLine(pendingWindowsUpdateMessage);
 
             Assert.AreEqual(expectedValue, pendingUpdates, "Unexpected value for {0}: {1}", dateToTest, pendingUpdates);
         }
@@ -36,15 +39,17 @@ namespace PRISMTest
         [TestCase("8/13/2017 3 am", true)]
         [TestCase("8/13/2017 6:11 am", true)]
         [TestCase("8/13/2017 9 am", false)]
-        [TestCase("8/13/2017 10 am", true)]
+        [TestCase("8/13/2017 10 am", false)]        // Was true prior to September 2019
         [TestCase("8/13/2017 11:30 am", false)]
         [TestCase("8/14/2017 2 am", false)]
         [TestCase("8/17/2017 2 am", false)]
         public void ServerUpdatesArePending(string dateToTest, bool expectedValue)
         {
-
             var currentDate = DateTime.Parse(dateToTest);
-            var pendingUpdates = WindowsUpdateStatus.ServerUpdatesArePending(currentDate, out _);
+            var pendingUpdates = WindowsUpdateStatus.ServerUpdatesArePending(currentDate, out var pendingWindowsUpdateMessage);
+
+            if (pendingUpdates)
+                Console.WriteLine(pendingWindowsUpdateMessage);
 
             Assert.AreEqual(expectedValue, pendingUpdates, "Unexpected value for {0}: {1}", dateToTest, pendingUpdates);
         }
