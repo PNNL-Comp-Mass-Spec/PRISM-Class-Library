4d23cf024a9c29dc3d1adb2d8764d79e0d4f5ef8
alchemistmatt <matthew.monroe@pnnl.gov>
2020-02-11 18:43:03 -0800

    Update logic used to determine the database type given a connection string

--- PRISMDatabaseUtils/DbToolsFactory.cs
+++ PRISMDatabaseUtils/DbToolsFactory.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+using Npgsql;
 using PRISMDatabaseUtils.PostgresSQL;
 
 namespace PRISMDatabaseUtils
@@ -30,23 +35,102 @@ namespace PRISMDatabaseUtils
         }
 
         /// <summary>
+        /// Set to true once mConnectionStringKeywordMap has been initialized
+        /// </summary>
+        private static bool mConnectionStringKeywordMapInitialized;
+
+        private static readonly Regex mDbServerTypeMatcher = new
+            Regex(@"DbServerType\s*=(?<ServerType>[a-z]+)\s*;?", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        /// <summary>
+        /// Map between Regex matchers and the server type for each Regex
+        /// </summary>
+        private static readonly List<KeyValuePair<Regex, DbServerTypes>> mConnectionStringKeywordMap = new List<KeyValuePair<Regex, DbServerTypes>>();
+
+        /// <summary>
         /// Checks elements in the connection string to determine which database engine it refers to.
         /// </summary>
         /// <param name="connectionString"></param>
         /// <returns></returns>
         public static DbServerTypes GetServerTypeFromConnectionString(string connectionString)
         {
-            if (connStr.IndexOf("Data Source", StringComparison.OrdinalIgnoreCase) > -1 || connStr.IndexOf("Integrated Security", StringComparison.OrdinalIgnoreCase) > -1 || connStr.IndexOf("Initial Catalog", StringComparison.OrdinalIgnoreCase) > -1)
+
+            // Example SQL Server connection string:
+            // "Data Source=MyServer;Initial Catalog=MyDatabase;integrated security=SSPI"
+
+            // Example PostgreSQL connection string:
+            // "Host=MyServer;Username=MyUser;Password=pass;Database=MyDatabase"
+
+            if (!mConnectionStringKeywordMapInitialized)
             {
-                return DbServerTypes.MSSQLServer;
+                InitializeConnectionStringKeywordMap();
             }
 
-            if (connStr.IndexOf("host", StringComparison.OrdinalIgnoreCase) > -1 || connStr.IndexOf("hostaddr", StringComparison.OrdinalIgnoreCase) > -1 || connStr.IndexOf("dbname", StringComparison.OrdinalIgnoreCase) > -1)
+            foreach (var keywordInfo in mConnectionStringKeywordMap)
             {
-                return DbServerTypes.PostgresSQL;
+                var match = keywordInfo.Key.Match(connectionString);
+
+                if (!match.Success)
+                {
+                    continue;
+                }
+
+                if (keywordInfo.Value != DbServerTypes.Undefined)
+                    return keywordInfo.Value;
+
+                // Successful match to DbServerType=...
+
+                // Assume DbServerType=SqlServer
+                // Assume DbServerType=MSSqlServer
+                // or     DbServerType=Postgres
+                // or     DbServerType=PostgreSQL
+
+                var serverType = match.Groups["ServerType"].Value;
+
+                if (serverType.Equals("SqlServer", StringComparison.OrdinalIgnoreCase) ||
+                    serverType.Equals("MSSqlServer", StringComparison.OrdinalIgnoreCase))
+                {
+                    return DbServerTypes.MSSQLServer;
+                }
+
+                if (serverType.Equals("Postgres", StringComparison.OrdinalIgnoreCase) ||
+                    serverType.Equals("PostgreSQL", StringComparison.OrdinalIgnoreCase))
+                {
+                    return DbServerTypes.PostgresSQL;
+                }
+
+                throw new Exception(string.Format("Invalid value for {0}; use SqlServer, MSSqlServer, Postgres, or PostgreSQL", match.Value));
+
+            }
+
+            try
+            {
+                var builder = new SqlConnectionStringBuilder(connectionString);
+                if (!string.IsNullOrWhiteSpace(builder.DataSource))
+                {
+                    return DbServerTypes.MSSQLServer;
+                }
+            }
+            catch
+            {
+                // Ignore errors here
+            }
+
+            try
+            {
+                var builder = new NpgsqlConnectionStringBuilder();
+                if (!string.IsNullOrWhiteSpace(builder.Host))
+                {
+                    return DbServerTypes.PostgresSQL;
+                }
+            }
+            catch
+            {
+                // Ignore errors here
             }
 
             return DbServerTypes.MSSQLServer;
+
         }
 
         /// <summary>
@@ -57,13 +141,22 @@ namespace PRISMDatabaseUtils
         /// <returns></returns>
         public static IDBTools GetDBTools(string connectionString, int timeoutSeconds = DbUtilsConstants.DEFAULT_SP_TIMEOUT_SEC)
         {
-            switch (GetServerTypeFromConnectionString(connStr))
+            var serverType = GetServerTypeFromConnectionString(connectionString);
+
+            // Remove "DbServerType=...;" if present
+            var standardConnectionString = mDbServerTypeMatcher.Replace(connectionString, string.Empty);
+
+            switch (serverType)
             {
                 case DbServerTypes.PostgresSQL:
-                    return new PostgresDBTools(connStr, timeoutSeconds);
-                case DbServerTypes.MSSQLServer:
+                    return new PostgresDBTools(standardConnectionString, timeoutSeconds);
+
                 default:
-                    return new SQLServerDBTools(connStr, timeoutSeconds);
+                    // Includes DbServerTypes.MSSQLServer
+                    return new SQLServerDBTools(standardConnectionString, timeoutSeconds);
+            }
+        }
+
             }
         }
 
@@ -91,5 +184,30 @@ namespace PRISMDatabaseUtils
 
             return SqlType.VarChar;
         }
+
+        private static void InitializeConnectionStringKeywordMap()
+        {
+            mConnectionStringKeywordMap.Clear();
+
+            // This is a special case that will be resolved by GetServerTypeFromConnectionString
+            // DbServerType is a non-standard connection string keyword, which allows the user to explicitly specify the database server type
+            mConnectionStringKeywordMap.Add(new KeyValuePair<Regex, DbServerTypes>(mDbServerTypeMatcher, DbServerTypes.Undefined));
+
+            InitializeKeywordInfo(@"Data Source\s*=", DbServerTypes.MSSQLServer);
+            InitializeKeywordInfo(@"Initial Catalog\s*=", DbServerTypes.MSSQLServer);
+            InitializeKeywordInfo(@"host\s*=", DbServerTypes.PostgresSQL);
+            InitializeKeywordInfo(@"db\s*=", DbServerTypes.PostgresSQL);
+
+            mConnectionStringKeywordMapInitialized = true;
+        }
+
+        private static void InitializeKeywordInfo(string matchSpec, DbServerTypes serverType)
+        {
+            var keywordMatcher = new Regex(matchSpec, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            var keywordInfo = new KeyValuePair<Regex, DbServerTypes>(keywordMatcher, serverType);
+            mConnectionStringKeywordMap.Add(keywordInfo);
+        }
+
     }
 }
