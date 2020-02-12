04d40f86e332c2b98bb7cc96d8f810c41b11c5c2
alchemistmatt <matthew.monroe@pnnl.gov>
2020-02-11 18:42:06 -0800

    Add DbServerTypes.Undefined enum

--- PRISMDatabaseUtils/DbToolsFactory.cs
+++ PRISMDatabaseUtils/DbToolsFactory.cs
@@ -14,6 +14,11 @@ namespace PRISMDatabaseUtils
         public enum DbServerTypes
         {
             /// <summary>
+            /// Undefined server type
+            /// </summary>
+            Undefined,
+
+            /// <summary>
             /// Microsoft SQL Server
             /// </summary>
             MSSQLServer,
