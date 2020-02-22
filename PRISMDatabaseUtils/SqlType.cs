20f778c429c70fd3016e76a7f062fa9ff391a155
alchemistmatt <matthew.monroe@pnnl.gov>
2020-02-21 17:19:03 -0800

    Expand the types for enum SqlType
    Note: enum backing values have changed

--- PRISMDatabaseUtils/SqlType.cs
+++ PRISMDatabaseUtils/SqlType.cs
@@ -3,57 +3,166 @@
     /// <summary>
     /// Commonly used SQL database types; converted by implementations of IExecuteSP to the database-specific types
     /// </summary>
-    /// <remarks>This is used instead of System.Data.DbType to allow the commonly-used names with understandable behavior, instead of just Int32/String/etc., and things like 'Time' that maps to 'DateTime' and no option for only a 'Date'</remarks>
+    /// <remarks>
+    /// This enum used instead of System.Data.DbType to allow the commonly-used names with understandable behavior, instead of just Int32/String/etc.
+    /// This also allows us to differentiate between Time, Date, and DateTime.
+    /// If you need to access a data type not listed here, use this design pattern:
+    ///
+    ///   var newParam = dbTools.AddParameter(cmd, argName, SqlType.Real);
+    ///   if (newParam is NpgsqlParameter sqlParam)
+    ///   {
+    ///       sqlParam.NpgsqlDbType = NpgsqlDbType.Range;
+    ///   }
+    /// </remarks>
     public enum SqlType
     {
         /// <summary>
-        /// 4-byte int
+        /// In SQL Server, values in a bit column can be 0, 1, or Null
+        /// In PostgreSQL, values in a bit column hold a string of 1's and 0's
+        /// </summary>
+        Bit,
+
+        /// <summary>
+        /// 1-byte state of true or false
+        /// Specific to PostgreSQL
+        /// </summary>
+        Boolean,
+
+        /// <summary>
+        /// 1-byte integer
+        /// </summary>
+        TinyInt,
+
+        /// <summary>
+        /// 2-byte integer
+        /// </summary>
+        SmallInt,
+
+        /// <summary>
+        /// 4-byte integer
         /// </summary>
         Int,
+
         /// <summary>
-        /// 8-byte int
+        /// 4-byte integer
+        /// </summary>
+        Integer = Int,
+
+        /// <summary>
+        /// 8-byte integer
         /// </summary>
         BigInt,
+
         /// <summary>
         /// 4-byte floating point value
         /// </summary>
         Real,
+
         /// <summary>
         /// 8-byte floating point value
         /// </summary>
         Float,
+
         /// <summary>
-        /// 1-byte int
+        /// 8-byte floating point value
+        /// The name Double is specific to PostgreSQL
         /// </summary>
-        TinyInt,
+        Double = Float,
+
         /// <summary>
-        /// 2-byte int
+        /// Floating point number with a specific precision and scale
         /// </summary>
-        SmallInt,
+        Decimal,
+
         /// <summary>
-        /// fixed-length string (with size)
+        /// Floating point number with a specific precision and scale
+        /// Synonymous with Decimal
+        /// </summary>
+        Numeric = Decimal,
+
+        /// <summary>
+        /// 8-byte floating point value
+        /// </summary>
+        Money,
+
+        /// <summary>
+        /// Fixed-length string (with size)
         /// </summary>
         Char,
+
         /// <summary>
-        /// variable-length string, with optional size limit
+        /// Variable-length string, with optional size limit
         /// </summary>
         VarChar,
+
         /// <summary>
-        /// variable-length string
+        /// Variable-length string
         /// </summary>
         Text,
+
         /// <summary>
-        /// Date
+        /// Case-insensitive text
+        /// Specific to PostgreSQL
+        /// </summary>
+        Citext,
+
+        /// <summary>
+        /// Object name
+        /// Specific to PostgreSQL
+        /// </summary>
+        Name,
+
+        /// <summary>
+        /// Date (no time of day)
+        /// 4-bytes
         /// </summary>
         Date,
+
+        /// <summary>
+        /// Time of day (no date)
+        /// 8 bytes
+        /// </summary>
+        Time,
+
         /// <summary>
         /// Date and Time
         /// </summary>
         DateTime,
+
+        /// <summary>
+        /// Date and time
+        /// Specific to PostgreSQL
+        /// </summary>
+        Timestamp = DateTime,
+
+        /// <summary>
+        /// Date and Time, with timezone
+        /// Corresponds to DateTimeOffset in SQL Server
+        /// </summary>
+        TimestampTz,
+
+        /// <summary>
+        /// Time interval
+        /// Specific to PostgreSQL
+        /// </summary>
+        Interval,
+
+        /// <summary>
+        /// Universally Unique Identifier (aka GUID)
+        /// 16 bytes
+        /// </summary>
+        UUID,
+
         /// <summary>
         /// 4-byte int
         /// </summary>
         Xml,
 
+        /// <summary>
+        /// Used to store JSON (JavaScript Object Notation) data
+        /// Specific to PostgreSQL
+        /// </summary>
+        Json
     }
+
 }
