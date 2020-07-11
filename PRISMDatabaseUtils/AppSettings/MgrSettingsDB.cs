fbffa2224efcbd0075d5fe7fbf1a0e305378ec88
alchemistmatt <matthew.monroe@pnnl.gov>
2020-07-10 20:05:55 -0700

    Add method ValidatePgPass
    If a PostgreSQL connection string is defined, looks for the pgpass file in the platform-specific default location
    Shows the path to the file if found, or a warning message if not found

--- PRISMDatabaseUtils/AppSettings/MgrSettingsDB.cs
+++ PRISMDatabaseUtils/AppSettings/MgrSettingsDB.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
 using PRISM;
 using PRISM.AppSettings;
 
@@ -79,5 +82,86 @@ namespace PRISMDatabaseUtils.AppSettings
 
             return true;
         }
+
+        /// <summary>
+        /// Examine the settings in MgrParams to look for parameters MgrCnfgDbConnectStr and/or DefaultDMSConnString
+        /// If defined, and if pointing to a PostgreSQL server, look for a pgpass file for the current user
+        /// </summary>
+        public void ValidatePgPass()
+        {
+            ValidatePgPass(MgrParams);
+        }
+
+        /// <summary>
+        /// Examine configFileSettings to look for parameters MgrCnfgDbConnectStr and/or DefaultDMSConnString
+        /// If defined, and if pointing to a PostgreSQL server, look for a pgpass file for the current user
+        /// </summary>
+        /// <param name="configFileSettings"></param>
+        public void ValidatePgPass(IReadOnlyDictionary<string, string> configFileSettings)
+        {
+            var connectionStringSettingNames = new SortedSet<string>(StringComparer.OrdinalIgnoreCase)
+            {
+                MGR_PARAM_MGR_CFG_DB_CONN_STRING,
+                DEFAULT_DMS_CONN_STRING
+            };
+
+            ValidatePgPass(configFileSettings, connectionStringSettingNames);
+        }
+
+        /// <summary>
+        /// Examine configFileSettings to look for parameters in connectionStringParameterNames
+        /// If defined, and if pointing to a PostgreSQL server, look for a pgpass file for the current user
+        /// </summary>
+        /// <param name="configFileSettings"></param>
+        /// <param name="connectionStringParameterNames"></param>
+        public void ValidatePgPass(IReadOnlyDictionary<string, string> configFileSettings, SortedSet<string> connectionStringParameterNames)
+        {
+            // This is used to look for Integrated Security=true
+            var integratedSecurityMatcher = new Regex(@"Integrated Security\s*=\s*true", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            foreach (var settingName in connectionStringParameterNames)
+            {
+                if (!configFileSettings.TryGetValue(settingName, out var connectionString))
+                    continue;
+
+                var serverType = DbToolsFactory.GetServerTypeFromConnectionString(connectionString);
+
+                if (serverType != DbServerTypes.PostgreSQL)
+                    continue;
+
+                if (integratedSecurityMatcher.IsMatch(connectionString))
+                {
+                    // Will connect to the PostgreSQL server using integrity security; do not look for a pgpass file
+                    return;
+                }
+
+                FileInfo pgPassFile;
+                if (SystemInfo.IsLinux)
+                {
+                    // Standard location: ~/.pgpass
+                    var passwordFileName = ".pgpass";
+                    pgPassFile = new FileInfo(Path.Combine("~", passwordFileName));
+                }
+                else
+                {
+                    // ReSharper disable once CommentTypo
+                    // Standard location: %APPDATA%\postgresql\pgpass.conf
+                    var passwordFileName = "pgpass.conf";
+                    var appDataDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                    pgPassFile = new FileInfo(Path.Combine(appDataDirectory, "postgresql", passwordFileName));
+                }
+
+
+                if (pgPassFile.Exists)
+                {
+                    Console.WriteLine("Postgres user password will be read from " + pgPassFile.FullName);
+                    return;
+                }
+
+                OnWarningEvent("Postgres pgpass file not found; expected to find it at " + pgPassFile.FullName);
+                return;
+            }
+        }
+
     }
 }
