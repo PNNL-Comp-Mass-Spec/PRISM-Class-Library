b6232c1eb722e50fc907d226c8d9148181ec68ed
alchemistmatt <matthew.monroe@pnnl.gov>
2021-03-18 22:02:34 -0700

    Suppress warnings

---
+++ PRISMWin/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Design", "RCS1075:Avoid empty catch clause that catches System.Exception.", Justification = "Ignore errors here", Scope = "member", Target = "~M:PRISMWin.ProcessStats.GetCPUUtilization~System.Single")]
+[assembly: SuppressMessage("Design", "RCS1075:Avoid empty catch clause that catches System.Exception.", Justification = "Ignore errors here", Scope = "member", Target = "~M:PRISMWin.ProcessStats.ClearCachedPerformanceCounterForProcessID(System.Int32)")]
--- UnitTests/GlobalSuppressions.cs
+++ UnitTests/GlobalSuppressions.cs
@@ -7,6 +7,7 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Not required", Scope = "module")]
 [assembly: SuppressMessage("General", "RCS1118:Mark local variable as const.", Justification = "Acceptable design pattern", Scope = "module")]
-[assembly: SuppressMessage("Usage", "RCS1246:Use element access.", Justification = "Acceptable", Scope = "member", Target = "~M:PRISMTest.TestDBTools.TestGetColumnValue(System.String,System.String,System.Int32)")]
-[assembly: SuppressMessage("Usage", "RCS1246:Use element access.", Justification = "Acceptable", Scope = "member", Target = "~M:PRISMTest.TestDBTools.TestQueryFailures(System.String,System.String,System.String,System.String,System.String,System.Int32,System.String)")]
-[assembly: SuppressMessage("Usage", "RCS1246:Use element access.", Justification = "Acceptable", Scope = "member", Target = "~M:PRISMTest.TestDBTools.TestQueryTableWork(System.String,System.String,System.String,System.String,System.Int32,System.String)")]
+[assembly: SuppressMessage("Usage", "RCS1246:Use element access.", Justification = "Prefer to use .First()", Scope = "member", Target = "~M:PRISMTest.TestDBTools.TestGetColumnValue(System.String,System.String,System.Int32)")]
+[assembly: SuppressMessage("Usage", "RCS1246:Use element access.", Justification = "Prefer to use .First()", Scope = "member", Target = "~M:PRISMTest.TestDBTools.TestQueryFailures(System.String,System.String,System.String,System.String,System.String,System.Int32,System.String)")]
+[assembly: SuppressMessage("Usage", "RCS1246:Use element access.", Justification = "Prefer to use .First()", Scope = "member", Target = "~M:PRISMTest.TestDBTools.TestQueryTableWork(System.String,System.String,System.String,System.String,System.Int32,System.String)")]
+[assembly: SuppressMessage("Usage", "RCS1246:Use element access.", Justification = "Prefer to use .First()", Scope = "member", Target = "~M:PRISMTest.TestLinuxSystemInfo.TestGetCoreUsageByProcessName(System.String,System.Int32,System.String,System.String,System.Double)")]
