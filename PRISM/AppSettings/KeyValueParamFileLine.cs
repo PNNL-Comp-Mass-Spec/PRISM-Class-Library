342d510ed6b26bd9244d3cb1c121798882011008
alchemistmatt <matthew.monroe@pnnl.gov>
2021-08-24 19:06:28 -0700

    Update KeyValueParamFileLine to track comments

--- PRISM/AppSettings/KeyValueParamFileLine.cs
+++ PRISM/AppSettings/KeyValueParamFileLine.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections.Generic;
 
 // ReSharper disable UnusedMember.Global
+
 namespace PRISM.AppSettings
 {
     /// <summary>
-    /// Line of data from a Key=Value parameter file
-    /// The data line optionally has a parameter name and value
+    /// Line of data from a Key=Value parameter file, stored in<see cref="Text"/>
     /// </summary>
+    /// <remarks>
+    /// The class optionally also tracks the parameter name and value, using <see cref="ParamName"/> and <see cref="ParamValue"/>
+    /// </remarks>
     public class KeyValueParamFileLine
     {
         /// <summary>
@@ -30,6 +33,12 @@ namespace PRISM.AppSettings
         public string ParamValue { get; private set; }
 
         /// <summary>
+        /// Comment text; may be an empty string
+        /// </summary>
+        /// <remarks>If a comment is defined, this includes the leading # comment character</remarks>
+        public string Comment { get; private set; }
+
+        /// <summary>
         /// True if ParamName has text, otherwise false
         /// </summary>
         /// <remarks>If ParamName has text but ParamValue is empty, this still returns true</remarks>
@@ -40,52 +49,114 @@ namespace PRISM.AppSettings
         /// </summary>
         /// <param name="lineNumber"></param>
         /// <param name="lineText"></param>
-        public KeyValueParamFileLine(int lineNumber, string lineText)
+        /// <param name="parseKeyValuePair">When true, parse lineText to determine the key name and value</param>
+        public KeyValueParamFileLine(int lineNumber, string lineText, bool parseKeyValuePair = false)
         {
             LineNumber = lineNumber;
             Text = lineText;
-            ParamName = string.Empty;
-            ParamValue = string.Empty;
+
+            if (!parseKeyValuePair)
+            {
+                ParamName = string.Empty;
+                ParamValue = string.Empty;
+
+                return;
+            }
+
+            var parsedSetting = KeyValueParamFileReader.GetKeyValueSetting(lineText, out var comment);
+
+            ParamName = parsedSetting.Key;
+            ParamValue = parsedSetting.Value;
+            StoreComment(comment);
         }
 
         /// <summary>
-        /// Constructor that just an instance of this class
+        /// Constructor that just takes an instance of this class
         /// </summary>
         /// <param name="paramFileLine"></param>
         public KeyValueParamFileLine(KeyValueParamFileLine paramFileLine) : this(paramFileLine.LineNumber, paramFileLine.Text)
         {
             ParamName = paramFileLine.ParamName;
             ParamValue = paramFileLine.ParamValue;
+            StoreComment(paramFileLine.Comment);
+        }
+
+        private void StoreComment(string comment)
+        {
+            if (string.IsNullOrWhiteSpace(comment))
+            {
+                Comment = string.Empty;
+                return;
+            }
+
+            var trimmedComment = comment.Trim();
+
+            if (trimmedComment.StartsWith("#"))
+            {
+                Comment = trimmedComment;
+                return;
+            }
+
+            Comment = string.Format("# {0}", trimmedComment);
         }
 
         /// <summary>
         /// Associate a parameter with this data line
         /// </summary>
-        /// <param name="paramName"></param>
-        /// <param name="paramValue"></param>
-        public void StoreParameter(string paramName, string paramValue)
+        /// <param name="paramName">Parameter name</param>
+        /// <param name="paramValue">Parameter value</param>
+        /// <param name="comment">Optional comment</param>
+        /// <param name="updateTextProperty">When true, update <see cref="Text"/></param>
+        public void StoreParameter(string paramName, string paramValue, string comment = "", bool updateTextProperty = false)
         {
             ParamName = paramName;
             ParamValue = paramValue;
+            StoreComment(comment);
+
+            if (updateTextProperty)
+                UpdateTextUsingStoredData();
         }
 
         /// <summary>
         /// Associate a parameter with this data line
         /// </summary>
-        /// <param name="paramInfo"></param>
-        public void StoreParameter(KeyValuePair<string, string> paramInfo)
+        /// <param name="paramInfo">Parameter</param>
+        /// <param name="comment">Optional comment</param>
+        /// <param name="updateTextProperty">When true, update <see cref="Text"/></param>
+        public void StoreParameter(KeyValuePair<string, string> paramInfo, string comment = "", bool updateTextProperty = false)
         {
             ParamName = paramInfo.Key;
             ParamValue = paramInfo.Value;
+            StoreComment(comment);
+
+            if (updateTextProperty)
+                UpdateTextUsingStoredData();
+        }
+
+        /// <summary>
+        /// Update property <see cref="Text"/> using <see cref="ParamName"/>, <see cref="ParamValue"/>, and <see cref="Comment"/>
+        /// </summary>
+        private void UpdateTextUsingStoredData()
+        {
+            Text = string.Format("{0}={1}{2}",
+                ParamName,
+                ParamValue,
+                string.IsNullOrWhiteSpace(Comment) ?
+                    string.Empty :
+                    string.Format("    {0}", Comment));
         }
 
         /// <summary>
         /// Update the value for this parameter
         /// </summary>
         /// <param name="value"></param>
-        protected void UpdateValue(string value)
+        /// <param name="updateTextProperty">When true, update <see cref="Text"/></param>
+        protected void UpdateValue(string value, bool updateTextProperty = false)
         {
             ParamValue = value ?? string.Empty;
+
+            if (updateTextProperty)
+                UpdateTextUsingStoredData();
         }
 
         /// <summary>
