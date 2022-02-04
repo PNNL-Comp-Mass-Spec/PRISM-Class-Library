5364f460ee02e5b5e5921d91c90ffaa7bcce17fa
alchemistmatt <matthew.monroe@pnnl.gov>
2022-02-03 21:58:18 -0800

    Add test for renaming a file when the target path already exists

--- UnitTests/FileRenameTests.cs
+++ UnitTests/FileRenameTests.cs
@@ -15,10 +15,23 @@ namespace PRISMTest
             mFileTools = new FileTools("PrismUnitTests", 1);
         }
 
+        private void CreateTestFile(FileInfo fileToRename, FileInfo newFile)
+        {
+            var changingDirectory = !string.Equals(fileToRename.DirectoryName, newFile.DirectoryName);
+
+            mFileTools.CreateDirectoryIfNotExists(fileToRename.DirectoryName);
+
+            using var writer = new StreamWriter(new FileStream(fileToRename.FullName, FileMode.Create, FileAccess.Write, FileShare.Read));
+
+            writer.WriteLine("Test file to be renamed from {0} to {1}",
+                changingDirectory ? fileToRename.FullName : fileToRename.Name,
+                changingDirectory ? newFile.FullName : newFile.Name);
+        }
+
         /// <summary>
         /// Test renaming a file, including waiting for a locked file to be released by another thread
         /// </summary>
-        /// <param name="targetFilePath"></param>
+        /// <param name="sourceFilePath"></param>
         /// <param name="newFileName"></param>
         /// <param name="retryCount"></param>
         /// <param name="secondsToLockFile"></param>
@@ -27,13 +40,13 @@ namespace PRISMTest
         [TestCase(@"C:\Temp\PrismLibraryTestFileC.txt", "PrismLibraryTestFileD.txt", 3, 3)]
         [TestCase(@"C:\Temp\PrismLibraryTestFileE.txt", "PrismLibraryTestFileF.txt", 3, 7)]
         [TestCase(@"C:\Temp\PrismLibraryTestFileG.txt", "PrismLibraryTestFileH.txt", 2, 15, false)]
-        public void RenameFile(string targetFilePath, string newFileName, int retryCount, int secondsToLockFile, bool expectSuccessfulRename = true)
+        public void RenameFile(string sourceFilePath, string newFileName, int retryCount, int secondsToLockFile, bool expectSuccessfulRename = true)
         {
-            var fileToRename = new FileInfo(targetFilePath);
+            var fileToRename = new FileInfo(sourceFilePath);
 
             if (fileToRename.Directory == null)
             {
-                Assert.Fail("Unable to determine the parent directory of file " + targetFilePath);
+                Assert.Fail("Unable to determine the parent directory of file " + sourceFilePath);
             }
 
             var newFileInfo = new FileInfo(Path.Combine(fileToRename.Directory.FullName, newFileName));
@@ -44,7 +57,7 @@ namespace PRISMTest
         /// <summary>
         /// Test renaming a file, moving it to a new directory, including waiting for a locked file to be released by another thread
         /// </summary>
-        /// <param name="targetFilePath"></param>
+        /// <param name="sourceFilePath"></param>
         /// <param name="newFilePath"></param>
         /// <param name="retryCount"></param>
         /// <param name="secondsToLockFile"></param>
@@ -53,13 +66,13 @@ namespace PRISMTest
         [TestCase(@"C:\Temp\PrismLibraryTestFileC.txt", @"C:\Temp\Renamed\PrismLibraryTestFileD.txt", 3, 3)]
         [TestCase(@"C:\Temp\PrismLibraryTestFileE.txt", @"C:\Temp\Renamed\PrismLibraryTestFileF.txt", 3, 7)]
         [TestCase(@"C:\Temp\PrismLibraryTestFileG.txt", @"C:\Temp\Renamed\PrismLibraryTestFileH.txt", 2, 15, false)]
-        public void RenameFileFullPath(string targetFilePath, string newFilePath, int retryCount, int secondsToLockFile, bool expectSuccessfulRename = true)
+        public void RenameFileFullPath(string sourceFilePath, string newFilePath, int retryCount, int secondsToLockFile, bool expectSuccessfulRename = true)
         {
-            var fileToRename = new FileInfo(targetFilePath);
+            var fileToRename = new FileInfo(sourceFilePath);
 
             if (fileToRename.Directory == null)
             {
-                Assert.Fail("Unable to determine the parent directory of file " + targetFilePath);
+                Assert.Fail("Unable to determine the parent directory of file " + sourceFilePath);
             }
 
             var newFileInfo = new FileInfo(newFilePath);
@@ -67,13 +80,49 @@ namespace PRISMTest
             RenameFileWork(fileToRename, newFileInfo, retryCount, secondsToLockFile, expectSuccessfulRename);
         }
 
+        /// <summary>
+        /// Test renaming a file when the target file already exists
+        /// </summary>
+        /// <param name="sourceFilePath"></param>
+        /// <param name="newFilePath"></param>
+        [TestCase(@"C:\Temp\PrismLibraryTestFileM.txt", @"C:\Temp\PrismLibraryTestFileN.txt")]
+        [TestCase(@"C:\Temp\PrismLibraryTestFileO.txt", @"C:\Temp\Renamed\PrismLibraryTestFileP.txt")]
+        public void RenameFileTestConflicts(string sourceFilePath, string newFilePath)
+        {
+            var fileToRename = new FileInfo(sourceFilePath);
+
+            if (fileToRename.Directory == null)
+            {
+                Assert.Fail("Unable to determine the parent directory of file " + sourceFilePath);
+            }
+
+            var newFileInfo = new FileInfo(newFilePath);
+
+            if (!fileToRename.Exists)
+            {
+                CreateTestFile(fileToRename, newFileInfo);
+            }
+
+            if (!newFileInfo.Exists)
+            {
+                using var writer = new StreamWriter(new FileStream(newFileInfo.FullName, FileMode.Create, FileAccess.Write, FileShare.Read));
+
+                writer.WriteLine("Test conflicting name file to prevent rename of " + newFileInfo.Name);
+            }
+
+            var success = mFileTools.RenameFileWithRetry(fileToRename, newFileInfo, out var errorMessage);
+
+            Assert.IsFalse(success, "RenameFileWithRetry returned true instead of false");
+
+            Console.WriteLine("As expected, the rename failed");
+            Console.WriteLine(errorMessage);
+        }
+
         private void RenameFileWork(FileInfo fileToRename, FileInfo newFile, int retryCount, int secondsToLockFile, bool expectSuccessfulRename = true)
         {
             if (!fileToRename.Exists)
             {
-                using var writer = new StreamWriter(new FileStream(fileToRename.FullName, FileMode.Create, FileAccess.Write, FileShare.Read));
-
-                writer.WriteLine("Test file to be renamed from {0} to {1}", fileToRename.Name, fileToRename.Name);
+                CreateTestFile(fileToRename, newFile);
             }
 
             if (newFile.Exists)
