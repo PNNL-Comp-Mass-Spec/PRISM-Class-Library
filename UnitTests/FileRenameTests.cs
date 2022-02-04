be94fd2c4f41caa2e09d47132cd1a9b81a60f837
alchemistmatt <matthew.monroe@pnnl.gov>
2022-02-03 18:42:03 -0800

    Add unit test that moves a file while renaming

--- UnitTests/FileRenameTests.cs
+++ UnitTests/FileRenameTests.cs
@@ -29,36 +29,65 @@ namespace PRISMTest
         [TestCase(@"C:\Temp\PrismLibraryTestFileG.txt", "PrismLibraryTestFileH.txt", 2, 15, false)]
         public void RenameFile(string targetFilePath, string newFileName, int retryCount, int secondsToLockFile, bool expectSuccessfulRename = true)
         {
-            var targetFile = new FileInfo(targetFilePath);
+            var fileToRename = new FileInfo(targetFilePath);
 
-            if (targetFile.Directory == null)
+            if (fileToRename.Directory == null)
             {
                 Assert.Fail("Unable to determine the parent directory of file " + targetFilePath);
             }
 
-            if (!targetFile.Directory.Exists)
-                mFileTools.CreateDirectoryIfNotExists(targetFile.Directory.FullName);
+            var newFileInfo = new FileInfo(Path.Combine(fileToRename.Directory.FullName, newFileName));
 
-            if (!targetFile.Exists)
+            RenameFileWork(fileToRename, newFileInfo, retryCount, secondsToLockFile, expectSuccessfulRename);
+        }
+
+        /// <summary>
+        /// Test renaming a file, moving it to a new directory, including waiting for a locked file to be released by another thread
+        /// </summary>
+        /// <param name="targetFilePath"></param>
+        /// <param name="newFilePath"></param>
+        /// <param name="retryCount"></param>
+        /// <param name="secondsToLockFile"></param>
+        /// <param name="expectSuccessfulRename"></param>
+        [TestCase(@"C:\Temp\PrismLibraryTestFileA.txt", @"C:\Temp\Renamed\PrismLibraryTestFileB.txt", 3, 0)]
+        [TestCase(@"C:\Temp\PrismLibraryTestFileC.txt", @"C:\Temp\Renamed\PrismLibraryTestFileD.txt", 3, 3)]
+        [TestCase(@"C:\Temp\PrismLibraryTestFileE.txt", @"C:\Temp\Renamed\PrismLibraryTestFileF.txt", 3, 7)]
+        [TestCase(@"C:\Temp\PrismLibraryTestFileG.txt", @"C:\Temp\Renamed\PrismLibraryTestFileH.txt", 2, 15, false)]
+        public void RenameFileFullPath(string targetFilePath, string newFilePath, int retryCount, int secondsToLockFile, bool expectSuccessfulRename = true)
+        {
+            var fileToRename = new FileInfo(targetFilePath);
+
+            if (fileToRename.Directory == null)
             {
-                using var writer = new StreamWriter(new FileStream(targetFile.FullName, FileMode.Create, FileAccess.Write, FileShare.Read));
-
-                writer.WriteLine("Test file to be renamed from {0} to {1}", targetFile.Name, newFileName);
+                Assert.Fail("Unable to determine the parent directory of file " + targetFilePath);
             }
 
-            var newFile = new FileInfo(Path.Combine(targetFile.Directory.FullName, newFileName));
+            var newFileInfo = new FileInfo(newFilePath);
+
+            RenameFileWork(fileToRename, newFileInfo, retryCount, secondsToLockFile, expectSuccessfulRename);
+        }
+
+        private void RenameFileWork(FileInfo fileToRename, FileInfo newFile, int retryCount, int secondsToLockFile, bool expectSuccessfulRename = true)
+        {
+            if (!fileToRename.Exists)
+            {
+                using var writer = new StreamWriter(new FileStream(fileToRename.FullName, FileMode.Create, FileAccess.Write, FileShare.Read));
+
+                writer.WriteLine("Test file to be renamed from {0} to {1}", fileToRename.Name, fileToRename.Name);
+            }
 
             if (newFile.Exists)
             {
                 Console.WriteLine("Deleting existing target file: " + newFile.FullName);
                 newFile.Delete();
                 Console.WriteLine();
+                newFile.Refresh();
             }
 
-            Console.WriteLine("Renaming {0} to {1}", targetFile.FullName, newFile.Name);
+            Console.WriteLine("Renaming {0} to {1}", fileToRename.FullName, newFile.FullName);
 
             var lockedReader = secondsToLockFile > 0
-                ? new FileLockUtility(targetFile.FullName, secondsToLockFile)
+                ? new FileLockUtility(fileToRename.FullName, secondsToLockFile)
                 : new FileLockUtility(string.Empty, secondsToLockFile);
 
             if (secondsToLockFile > 0)
@@ -69,7 +98,7 @@ namespace PRISMTest
                 ConsoleMsgUtils.SleepSeconds(1.5);
             }
 
-            var success = mFileTools.RenameFileWithRetry(targetFile, newFileName, retryCount, out var errorMessage);
+            var success = mFileTools.RenameFileWithRetry(fileToRename, newFile, out var errorMessage, retryCount);
 
             Console.WriteLine();
 
@@ -93,11 +122,12 @@ namespace PRISMTest
             }
 
             newFile.Refresh();
-
             if (!newFile.Exists)
             {
                 Assert.Fail("Renamed file not found: " + newFile.FullName);
             }
+
+            Assert.AreEqual(fileToRename.FullName, newFile.FullName);
 
             Console.WriteLine("Successfully renamed the file to " + newFile.FullName);
         }
