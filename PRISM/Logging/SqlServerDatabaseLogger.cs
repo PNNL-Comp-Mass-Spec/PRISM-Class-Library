db8c5d727a262cd7f8aafc45f686dfcddfa223af
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2024-11-20 18:24:18 -0800

    Make the old SQLServerDatabaseLogger class mostly a stub
    We generally are using the PostgresDatabaseLogger, so it shouldn't cause direct issues

--- PRISM/Logging/SqlServerDatabaseLogger.cs
+++ PRISM/Logging/SqlServerDatabaseLogger.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Concurrent;
-using System.Data;
-using System.Threading;
-using Microsoft.Data.SqlClient;
 
 namespace PRISM.Logging
 {
@@ -11,7 +7,7 @@ namespace PRISM.Logging
     /// Connect using Microsoft.Data.SqlClient
     /// </summary>
     /// <remarks>Can only log to a single database at a time</remarks>
-    [Obsolete("Use PRISMDatabaseUtils.Logging.SQLServerDatabaseLogger instead (drop-in replacement)")]
+    [Obsolete("Use PRISMDatabaseUtils.Logging.SQLServerDatabaseLogger instead (drop-in replacement); this class is only stubs now")]
     public sealed class SQLServerDatabaseLogger : DatabaseLogger
     {
         private const string DEFAULT_STORED_PROCEDURE_NAME = "post_log_entry";
@@ -19,39 +15,11 @@ namespace PRISM.Logging
         private const string DEFAULT_PARAM_NAME_MESSAGE = "message";
         private const string DEFAULT_PARAM_NAME_POSTED_BY = "postedBy";
 
-        private static readonly ConcurrentQueue<LogMessage> mMessageQueue = new();
-
-        private static readonly object mMessageQueueLock = new();
-
-        // ReSharper disable once UnusedMember.Local
-        private static readonly Timer mQueueLogger = new(LogMessagesCallback, null, 500, LOG_INTERVAL_MILLISECONDS);
-
-        /// <summary>
-        /// Tracks the number of successive dequeue failures
-        /// </summary>
-        private static int mFailedDequeueEvents;
-
-        /// <summary>
-        /// Module name
-        /// </summary>
-        private static string mModuleName;
-
         /// <summary>
         /// Program name to pass to the PostedBy field when contacting the database
         /// </summary>
         /// <remarks>Will be auto-defined in LogQueuedMessages if blank</remarks>
-        public static string ModuleName
-        {
-            get
-            {
-                if (string.IsNullOrWhiteSpace(mModuleName))
-                {
-                    mModuleName = GetDefaultModuleName();
-                }
-                return mModuleName;
-            }
-            set => mModuleName = value;
-        }
+        public static string ModuleName { get; set; }
 
         // ReSharper disable once UnusedMember.Global
 
@@ -119,12 +87,6 @@ namespace PRISM.Logging
             string moduleName,
             string connectionString)
         {
-            ChangeConnectionInfo(
-                moduleName, connectionString,
-                DEFAULT_STORED_PROCEDURE_NAME,
-                DEFAULT_PARAM_NAME_LOG_TYPE,
-                DEFAULT_PARAM_NAME_MESSAGE,
-                DEFAULT_PARAM_NAME_POSTED_BY);
         }
 
         /// <summary>
@@ -153,14 +115,6 @@ namespace PRISM.Logging
             int messageParamSize = 4096,
             int postedByParamSize = 128)
         {
-            ModuleName = moduleName;
-
-            ConnectionString = connectionString;
-
-            LoggingProcedure.UpdateProcedureInfo(
-                storedProcedure,
-                logTypeParamName, messageParamName, postedByParamName,
-                logTypeParamSize, messageParamSize, postedByParamSize);
         }
 
         /// <summary>
@@ -177,134 +131,6 @@ namespace PRISM.Logging
         /// </remarks>
         public override void FlushPendingMessages()
         {
-            // Maximum time, in seconds, to continue to call StartLogQueuedMessages while the message queue is not empty
-            const int MAX_TIME_SECONDS = 5;
-
-            var startTime = DateTime.UtcNow;
-
-            while (DateTime.UtcNow.Subtract(startTime).TotalSeconds < MAX_TIME_SECONDS)
-            {
-                StartLogQueuedMessages();
-
-                if (mMessageQueue.IsEmpty)
-                    break;
-
-                AppUtils.SleepMilliseconds(10);
-            }
-        }
-
-        /// <summary>
-        /// Callback invoked by the mQueueLogger timer
-        /// </summary>
-        /// <param name="state">State</param>
-        private static void LogMessagesCallback(object state)
-        {
-            ShowTrace("SQLServerDatabaseLogger.mQueueLogger callback raised");
-            StartLogQueuedMessages();
-        }
-
-        private static void LogQueuedMessages()
-        {
-            try
-            {
-                if (mMessageQueue.IsEmpty || !HasConnectionInfo)
-                    return;
-
-                ShowTrace(string.Format("SQLServerDatabaseLogger connecting to {0}", ConnectionString));
-                var messagesWritten = 0;
-
-                using (var sqlConnection = new SqlConnection(ConnectionString))
-                {
-                    sqlConnection.Open();
-
-                    var spCmd = new SqlCommand(LoggingProcedure.ProcedureName)
-                    {
-                        CommandType = CommandType.StoredProcedure
-                    };
-
-                    spCmd.Parameters.Add(new SqlParameter("@Return", SqlDbType.Int)).Direction = ParameterDirection.ReturnValue;
-
-                    var logTypeParam = spCmd.Parameters.Add(new SqlParameter(LoggingProcedure.LogTypeParamName, SqlDbType.VarChar, LoggingProcedure.LogTypeParamSize));
-                    var logMessageParam = spCmd.Parameters.Add(new SqlParameter(LoggingProcedure.MessageParamName, SqlDbType.VarChar, LoggingProcedure.MessageParamSize));
-                    spCmd.Parameters.Add(new SqlParameter(LoggingProcedure.LogSourceParamName, SqlDbType.VarChar, LoggingProcedure.LogSourceParamSize)).Value = ModuleName;
-
-                    spCmd.Connection = sqlConnection;
-                    spCmd.CommandTimeout = TIMEOUT_SECONDS;
-
-                    var fatalError = false;
-
-                    while (!mMessageQueue.IsEmpty)
-                    {
-                        if (!mMessageQueue.TryDequeue(out var logMessage))
-                        {
-                            mFailedDequeueEvents++;
-                            LogDequeueError(mFailedDequeueEvents, mMessageQueue.Count);
-                            return;
-                        }
-
-                        mFailedDequeueEvents = 0;
-
-                        if (logMessage.LogLevel is LogLevels.ERROR or LogLevels.FATAL)
-                        {
-                            MostRecentErrorMessage = logMessage.Message;
-                        }
-
-                        if (logMessageParam == null)
-                            continue;
-
-                        logTypeParam.Value = LogLevelToString(logMessage.LogLevel);
-                        logMessageParam.Value = logMessage.Message;
-
-                        var retryCount = 2;
-
-                        while (retryCount > 0)
-                        {
-                            var returnValue = 0;
-
-                            try
-                            {
-                                spCmd.ExecuteNonQuery();
-                                returnValue = Convert.ToInt32(spCmd.Parameters["@Return"].Value);
-
-                                messagesWritten++;
-                                break;
-                            }
-                            catch (Exception ex)
-                            {
-                                --retryCount;
-                                var errorMessage = "Exception calling stored procedure " +
-                                                   spCmd.CommandText + ": " + ex.Message +
-                                                   "; resultCode = " + returnValue + "; Retry count = " + retryCount + "; " +
-                                                   StackTraceFormatter.GetExceptionStackTrace(ex);
-
-                                ConsoleMsgUtils.ShowWarning(errorMessage);
-
-                                if (retryCount == 0)
-                                    FileLogger.WriteLog(LogLevels.ERROR, errorMessage);
-                                else
-                                    FileLogger.WriteLog(LogLevels.WARN, errorMessage);
-
-                                if (ex.Message.StartsWith("Could not find stored procedure"))
-                                {
-                                    fatalError = true;
-                                    break;
-                                }
-                            }
-                        }
-
-                        if (fatalError)
-                            break;
-                    }
-                }
-
-                ShowTrace(string.Format("SQLServerDatabaseLogger connection closed; wrote {0} messages", messagesWritten));
-            }
-            catch (Exception ex)
-            {
-                ConsoleMsgUtils.ShowErrorCustom(
-                    "Error writing queued log messages to the database using SQL Server: " + ex.Message,
-                    ex, false, false);
-            }
         }
 
         /// <summary>
@@ -321,13 +147,6 @@ namespace PRISM.Logging
         /// </summary>
         private static void StartLogQueuedMessages()
         {
-            if (mMessageQueue.IsEmpty)
-                return;
-
-            lock (mMessageQueueLock)
-            {
-                LogQueuedMessages();
-            }
         }
 
         /// <summary>
@@ -336,10 +155,6 @@ namespace PRISM.Logging
         /// <param name="logMessage">Log message</param>
         public override void WriteLog(LogMessage logMessage)
         {
-            mMessageQueue.Enqueue(logMessage);
-
-            if (EchoMessagesToFileLogger)
-                FileLogger.WriteLog(logMessage);
         }
 
         /// <summary>
@@ -347,8 +162,6 @@ namespace PRISM.Logging
         /// </summary>
         ~SQLServerDatabaseLogger()
         {
-            ShowTrace("Disposing SQLServerDatabaseLogger");
-            StartLogQueuedMessages();
         }
     }
 }
